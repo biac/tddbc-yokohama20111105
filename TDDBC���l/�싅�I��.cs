a97f1a1ce51175daf5e9d90e931452089d819104
biac <biac@nifty.com>
2011-11-18 17:09:50 +0900

    お題その1 - テストケース2完

--- TDDBC���l/�싅�I��.cs
+++ TDDBC���l/�싅�I��.cs
@@ -1,7 +1,7 @@
 ﻿namespace TDDBC横浜 {
 	public class 野球選手 {
 		public decimal Calc打率(int 打席数, int 打数, int 安打数) {
-			return 0.0m;
+			return 安打数 / 打数;
 		}
 	}
 }
--- TDDBC���l/�싅�I��Tests.cs
+++ TDDBC���l/�싅�I��Tests.cs
@@ -3,7 +3,8 @@
 namespace TDDBC横浜 {
 	[TestFixture()]
 	public class 野球選手Tests {
-		[TestCase(0, 0, 0, 0.0)]
+		[TestCase(5, 3, 0, 0.0)]
+		[TestCase(5, 3, 3, 1.0)]
 		public void Calc打率Test01(int 打席数, int 打数, int 安打数, decimal 期待値) {
 			野球選手 p = new 野球選手();
 			decimal 打率 = p.Calc打率(打席数, 打数, 安打数);
