113a81ba3a59aeec5b344f993398576c8b891c7b
biac <biac@nifty.com>
2011-11-18 17:23:54 +0900

    お題その1 - テストケース3完(ゼロ除算例外の仕様化テスト)

--- TDDBC���l/�싅�I��Tests.cs
+++ TDDBC���l/�싅�I��Tests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace TDDBC横浜 {
 	[TestFixture()]
@@ -9,6 +10,12 @@ namespace TDDBC横浜 {
 			野球選手 p = new 野球選手();
 			decimal 打率 = p.Calc打率(打席数, 打数, 安打数);
 			Assert.AreEqual(期待値, 打率);
+		}
+
+		[TestCase(0, 0, 0)]
+		public void Calc打率Test02_打率0なら例外(int 打席数, int 打数, int 安打数) {
+			野球選手 p = new 野球選手();
+			Assert.Throws<DivideByZeroException>(() => p.Calc打率(打席数, 打数, 安打数));
 		}
 	}
 }
