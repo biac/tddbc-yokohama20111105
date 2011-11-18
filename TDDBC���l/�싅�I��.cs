a87cde378684ca4857793bdce45c4ba318d613d0
biac <biac@nifty.com>
2011-11-18 17:42:48 +0900

    お題その1 - テストケース4(四捨五入)完

--- TDDBC���l/�싅�I��.cs
+++ TDDBC���l/�싅�I��.cs
@@ -1,7 +1,10 @@
-﻿namespace TDDBC横浜 {
+﻿using System;
+
+namespace TDDBC横浜 {
 	public class 野球選手 {
 		public decimal Calc打率(int 打席数, int 打数, int 安打数) {
-			return 安打数 / 打数;
+			return Math.Round((decimal)安打数 / 打数, 3, MidpointRounding.AwayFromZero);
+			//TODO: 四捨五入方式は AwayFromZero で良いか?
 		}
 	}
 }
--- TDDBC���l/�싅�I��Tests.cs
+++ TDDBC���l/�싅�I��Tests.cs
@@ -6,6 +6,7 @@ namespace TDDBC横浜 {
 	public class 野球選手Tests {
 		[TestCase(5, 3, 0, 0.0)]
 		[TestCase(5, 3, 3, 1.0)]
+		[TestCase(5, 9999, 2345, 0.235)]	//0.23452345…が四捨五入で0.235になる
 		public void Calc打率Test01(int 打席数, int 打数, int 安打数, decimal 期待値) {
 			野球選手 p = new 野球選手();
 			decimal 打率 = p.Calc打率(打席数, 打数, 安打数);
