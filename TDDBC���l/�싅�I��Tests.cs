44a04521d89a75fee6ddc83a15c753f7a1b73fff
biac <biac@nifty.com>
2011-11-19 10:27:15 +0900

    お題その3 - リファクタリング完

--- TDDBC���l/�싅�I��Tests.cs
+++ TDDBC���l/�싅�I��Tests.cs
@@ -7,16 +7,11 @@ namespace TDDBC横浜 {
 		[TestCase(5, 3, 0, 0.0)]
 		[TestCase(5, 3, 3, 1.0)]
 		[TestCase(5, 9999, 2345, 0.235)]	//0.23452345…が四捨五入で0.235になる
+		[TestCase(5, 0, 0, 0.0)]
 		public void Calc打率Test01(int 打席数, int 打数, int 安打数, decimal 期待値) {
 			野球選手 p = new 野球選手();
-			decimal 打率 = p.Calc打率(打席数, 打数, 安打数).Value;	// ".Value"を追加
+			decimal 打率 = p.Calc打率(打席数, 打数, 安打数).Value;
 			Assert.AreEqual(期待値, 打率);
-		}
-
-		[TestCase(5, 0, 0)]
-		public void Calc打率Test02_打数0なら0(int 打席数, int 打数, int 安打数) {
-			野球選手 p = new 野球選手();
-			Assert.AreEqual(0.0m, p.Calc打率(打席数, 打数, 安打数));
 		}
 
 		[TestCase(0, 0, 0, null)]
